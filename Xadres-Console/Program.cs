96d0c4a8c0310c345be61c0e8d9472f00d8667a3
Flavio Souza Silva <flavios@spi.local>
2017-09-27 14:44:06 -0300

    posicao

--- Xadres-Console/Program.cs
+++ Xadres-Console/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using tabuleiro;
 
 namespace Xadres_Console
 {
@@ -10,6 +11,11 @@ namespace Xadres_Console
     {
         static void Main(string[] args)
         {
+            Posicao P;
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("posição :" + P);
+            Console.ReadLine();
         }
     }
 }
