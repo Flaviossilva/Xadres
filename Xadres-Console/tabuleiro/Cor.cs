26c52d0444abd72269879f23bf2b7fc557efc0dc
Flavio Souza Silva <flavios@spi.local>
2017-09-27 15:44:48 -0300

    comecando a implementar o tabuleiro

---
+++ Xadres-Console/tabuleiro/Cor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tabuleiro
+{
+    enum Cor { 
+    
+    Branca,
+    Preta,
+    Vermelho,
+    Azul,
+    Amarelo,
+    Laranja,
+    Verde
+
+    }
+}
---
+++ Xadres-Console/tabuleiro/Peca.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tabuleiro
+{
+    class Peca
+    {
+        public Posicao posicao { get; set; }
+        public Cor cor { get; set; }
+        public int qtdMovimentos {get ; set;}
+        public Tabuleiro tab { get; protected set; }
+    }
+}
